9cd5da017c5815013f538deaa346d2c9fb250b95
qvdk <qvandekerckhove@gmail.com>
2019-12-02 19:09:54 +0100

    Use the new adviced Main class to launch cucumber from the package io.cucumber.core.cli

--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberFeatureLaunchConstants.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberFeatureLaunchConstants.java
@@ -6,7 +6,7 @@ public class CucumberFeatureLaunchConstants {
 	 // NO ENTRY, NO INSTANCES
 	}
 	public static final String CUCUMBER_FEATURE_RUNNER = "Cucumber Feature Runner";
-	public static final String CUCUMBER_API_CLI_MAIN = "cucumber.api.cli.Main";
+	public static final String CUCUMBER_API_CLI_MAIN = "io.cucumber.core.cli.Main";
 	public static final String ATTR_FEATURE_PATH = "cucumber feature";
 	public static final String ATTR_GLUE_PATH = "glue path";
 	public static final String DEFAULT_CLASSPATH = "classpath:";
